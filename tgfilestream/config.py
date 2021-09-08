7c1ad0ed630fcc4a93578891d2e6a6440cbc1a04
SilverHound27 <62786183+SilverHound27@users.noreply.github.com>
2021-09-08 20:46:25 +0530

    Update config.py

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -19,7 +19,7 @@ import os
 from yarl import URL
 
 try:
-    port = int(os.environ.get("PORT", "8080"))
+    port = 8080
 except ValueError:
     port = -1
 if not 1 <= port <= 65535:
@@ -27,33 +27,33 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["TG_API_ID"])
-    api_hash = os.environ["TG_API_HASH"]
+    api_id = 1786023
+    api_hash = "cd003a0cb8d10148cba0d1b404f2ba2c"
 except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
     sys.exit(1)
 
 trust_headers = bool(os.environ.get("TRUST_FORWARD_HEADERS"))
-host = os.environ.get("HOST", "localhost")
-public_url = URL(os.environ.get("PUBLIC_URL", f"http://{host}:{port}"))
+host = "0.0.0.0"  #"localhost"
+public_url = "https://potatostream.herokuapp.com/"
 
-session_name = os.environ.get("TG_SESSION_NAME", "tgfilestream")
-bot_token = os.environ.get("BOT_TOKEN", "tgfilestream")
+session_name = "tgfilestream"
+bot_token = "1889262909:AAFsEbC0fj7wF101DOkCgk1_OuNGsT_D0GI"
 
 log_config = os.environ.get("LOG_CONFIG")
 debug = bool(os.environ.get("DEBUG"))
 
 try:
     # The per-user ongoing request limit
-    request_limit = int(os.environ.get("REQUEST_LIMIT", "5"))
+    request_limit = 5
 except ValueError:
     print("Please make sure the REQUEST_LIMIT environment variable is an integer")
     sys.exit(1)
 
 try:
     # The per-DC connection limit
-    connection_limit = int(os.environ.get("CONNECTION_LIMIT", "20"))
+    connection_limit = 20
 except ValueError:
     print("Please make sure the CONNECTION_LIMIT environment variable is an integer")
     sys.exit(1)
